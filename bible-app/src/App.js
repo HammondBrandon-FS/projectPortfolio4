7287eb290e097a101f482f9aa4eb4c45b98a7b12
[brandon hammond] <[BHammond@student.fullsail.edu]>
2022-11-28 10:58:18 -0600

    Comments updated

--- bible-app/src/App.js
+++ bible-app/src/App.js
@@ -1,18 +1,23 @@
 // Parent Component - App.js
 
+// React Imports
 import React, { useState, useEffect } from 'react';
-
+// Components
 import Content from './components/Content';
 
 function App() {
+  // State to hold loaded bibles
   const [bibles, setBibles] = useState({
     items: []
   });
 
+  // Constants for API Information
   const url = 'https://api.scripture.api.bible/v1/bibles';
   const apiKey = '48393b9fab990a5b66b3225ed30151e2';
 
+  // getData connects to API asynchronously
   const getData = async () => {
+    // Pass API Key in header
     const response = await fetch(url, {
       method: 'get',
       headers: {
@@ -20,17 +25,20 @@ function App() {
       }
     });
     const data = await response.json();
+    // Set array in state
     setBibles({
       items: data.data
     });
   }
 
+  // getData after render using useEffect
   useEffect(() => {
     getData();
   }, [])
 
   return (
     <div style={styles.container}>
+      {/* Loop through array in state and output Content component for each item */}
       {bibles.items.map(bible => {
         return <Content key={bible.id} data={bible.name} />
       })}
@@ -40,6 +48,7 @@ function App() {
 
 export default App;
 
+// Styles
 const styles = {
   container: {
     background: '#e5e9f1'
--- bible-app/src/components/Content.js
+++ bible-app/src/components/Content.js
@@ -1,13 +1,16 @@
 //Child/Dummy Content.js
 //Parent - App.js
 
+// React Imports
 import React from 'react';
 
+// Data passed to component through props
 const Content = props => {
     return (
         <div style={styles.container}>
             <ul>
                 <li>
+                    {/* Output name of Bible in a list item */}
                     {props.data}
                 </li>
             </ul>
@@ -17,6 +20,7 @@ const Content = props => {
 
 export default Content;
 
+// Styles
 const styles = {
     container: {
         display: 'flex',
