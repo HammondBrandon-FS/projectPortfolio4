dbb9800127d73645e28da6766d45814bed453345
[brandon hammond] <[BHammond@student.fullsail.edu]>
2022-12-11 22:40:08 -0600

    Updates to Bible navigation

--- bible-app/src/components/Explore.js
+++ bible-app/src/components/Explore.js
@@ -4,13 +4,11 @@ import Chapter from '../components/Chapter';
 
 
 const Explore = props => {
-
-
     return (
         <div>
             <h2>Chapters</h2>
             {props.data.data.map(chapter => {
-                return <Chapter key={chapter.id} id={chapter.id} name={chapter.reference} setChapters={props.setChapters}/>
+                return <Chapter key={chapter.id} id={chapter.id} name={chapter.reference} url={props.url} setChapters={props.setChapters}/>
             })}
         </div>
     )
